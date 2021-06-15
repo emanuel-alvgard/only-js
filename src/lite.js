d6081eaa150b2d558eb2293b31a8f981a3c497b4
emanuel-alvgard <emanuel.alvgard@gmail.com>
2021-06-15 14:21:06 +0200

    -

--- src/lite.js
+++ src/lite.js
@@ -21,9 +21,9 @@ DOM_ROOT.style.padding = "0px";
 DOM_ROOT.style.border = "none";
 DOM_ROOT.style.left = "0px";
 DOM_ROOT.style.top = "0px";
-DOM_ROOT.style.width = "100%";
-DOM_ROOT.style.height = "100%";
-DOM_ROOT.style.zIndex = "0";
+DOM_ROOT.style.size_x = "100%";
+DOM_ROOT.style.size_y = "100%";
+DOM_ROOT.style.zIndex = "-1";
 DOM_ROOT.style.backgroundColor = "white";
 DOM_body.append(DOM_ROOT);
 
@@ -33,34 +33,48 @@ let DOM_element = [];
 let virtual_size = 0;
 let id;
 
-let width;
-let width_u;
+let size_x;
+let size_x_u;
 
-let height;
-let height_u;
+let size_y;
+let size_y_u;
 
 let x;
 let y;
-let rotation;
-let matrix_u;
+let rotation_z;
+let transform_u;
+
+let z;
+let z_u;
 
 
 
-function get_width(id) { return width[id]; }
-function set_width(id, value) { width[id] = value; width_u[id] = 1; }
+function get_size_x(id) { return size_x[id]; }
+function set_size_x(id, value) { size_x[id] = value; size_x_u[id] = 1; }
 
-function get_height(id) { return height[id]; }
-function set_height(id, value) { height[id] = value; height_u[id] = 1; }
+function get_size_y(id) { return size_y[id]; }
+function set_size_y(id, value) { size_y[id] = value; size_y_u[id] = 1; }
 
 function get_x(id) { return x[id]; }
-function set_x(id, value) { x[id] = value; matrix_u[id] = 1; }
+function set_x(id, value) { x[id] = value; transform_u[id] = 1; }
 
 function get_y(id) { return y[id]; }
-function set_y(id, value) { y[id] = value; matrix_u[id] = 1; }
+function set_y(id, value) { y[id] = value; transform_u[id] = 1; }
 
-function get_rotation(id) { return rotation[id]; }
-function set_rotation(id, value) { rotation[id] = value; matrix_u[id] = 1; }
+function get_rotation_z(id) { return rotation_z[id]; }
+function set_rotation_z(id, value) { rotation_z[id] = value; transform_u[id] = 1; }
 
+function get_z(id) { return z[id]; }
+function set_z(id, value) { z[id] = value; z_u[id] = 1; }
+
+
+
+function translate_x() {}
+function translate_y() {}
+function rotate_z() {}
+function translate_z() {}
+function scale_x() {}
+function scale_y() {}
 
 
 function create_virtual(size) {
@@ -68,21 +82,23 @@ function create_virtual(size) {
     virtual_size = size;
     id = new Int32Array(size);
 
-    width = new Int32Array(size);
-    width_u = new Int8Array(size);
+    size_x = new Int32Array(size);
+    size_x_u = new Int8Array(size);
     
-    height = new Int32Array(size);
-    height_u = new Int8Array(size);
+    size_y = new Int32Array(size);
+    size_y_u = new Int8Array(size);
     
     x = new Int32Array(size);
     y = new Int32Array(size);
-    rotation = new Int32Array(size);
-    matrix_u = new Int8Array(size);
+    rotation_z = new Int32Array(size);
+    transform_u = new Int8Array(size);
+
+    z = new Int32Array(size);
+    z_u = new Int8Array(size);
 
     let i = 0;
     while (i < size) { DOM_element[i] = document.createElement("div"); i += 1; } i = 0;
     while (i < size) { DOM_element[i].style.position = "absolute"; i += 1; } i = 0;
-    while (i < size) { DOM_element[i].style.zIndex = "1"; i += 1; } i = 0;
     while (i < size) { DOM_element[i].id = i; i += 1; } i = 0;
     while (i < size) { id[i] = i; i += 1; } i = 0;
     while (i < size) { ROOT.append(DOM_element[i]); i += 1; } i = 0;
@@ -93,16 +109,19 @@ function create_virtual(size) {
 function clear_virtual() {
     
     let i = 0;
-    while (i < virtual_size) { width[i] = 0; i += 1; } i = 0;
-    while (i < virtual_size) { width_u[i] = 1; i += 1; } i = 0;
+    while (i < virtual_size) { size_x[i] = 0; i += 1; } i = 0;
+    while (i < virtual_size) { size_x_u[i] = 1; i += 1; } i = 0;
 
-    while (i < virtual_size) { height[i] = 0; i += 1; } i = 0;
-    while (i < virtual_size) { height_u[i] = 1; i += 1; } i = 0;
+    while (i < virtual_size) { size_y[i] = 0; i += 1; } i = 0;
+    while (i < virtual_size) { size_y_u[i] = 1; i += 1; } i = 0;
 
     while (i < virtual_size) { x[i] = 0; i += 1; } i = 0;
     while (i < virtual_size) { y[i] = 0; i += 1; } i = 0;
-    while (i < virtual_size) { rotation[i] = 0; i += 1; } i = 0;
-    while (i < virtual_size) { matrix_u[i] = 1; i += 1; } i = 0;
+    while (i < virtual_size) { rotation_z[i] = 0; i += 1; } i = 0;
+    while (i < virtual_size) { transform_u[i] = 1; i += 1; } i = 0;
+
+    while (i < virtual_size) { z[i] = 0; i += 1; } i = 0;
+    while (i < virtual_size) { z_u[i] = 1; i += 1; } i = 0;
 }
 
 
@@ -113,30 +132,38 @@ function update_DOM() {
     
     i = 0;
     while (i < virtual_size) {
-        if (width_u[i] === 1) { 
-            DOM_element[i].style.width = width[i] + "px";
-            width_u[i] = 0;  
+        if (size_x_u[i] === 1) { 
+            DOM_element[i].style.width = size_x[i] + "px";
+            size_x_u[i] = 0;  
         } i += 1;
     } 
     
     i = 0;
     while (i < virtual_size) {
-        if (height_u[i] === 1) { 
-            DOM_element[i].style.height = height[i] + "px";
-            height_u[i] = 0; 
+        if (size_y_u[i] === 1) { 
+            DOM_element[i].style.height = size_y[i] + "px";
+            size_y_u[i] = 0; 
         } i += 1;
     } 
     
     i = 0;
     while (i < virtual_size) {
-        if (matrix_u[i] === 1) { 
+        if (transform_u[i] === 1) { 
             DOM_element[i].style.transform = "translate(" 
             + x[i] + "px, "
             + y[i] + "px) "
-            + "rotate(" + rotation[i] + "deg)";
-            matrix_u[i] = 0; 
+            + "rotate(" + rotation_z[i] + "deg)";
+            transform_u[i] = 0; 
         } i += 1;
     } i = 0;
+
+    i = 0;
+    while (i < virtual_size) {
+        if (z_u[i] === 1) { 
+            DOM_element[i].style.zIndex = z[i] + "";
+            z_u[i] = 0; 
+        } i += 1;
+    } 
 }
 
 
@@ -145,50 +172,45 @@ create_virtual(100);
 
 let box_1 = 0;
 let DOM_box_1 = DOM_element[box_1];
-set_width(box_1, 300);
-set_height(box_1, 300);
+set_size_x(box_1, 300);
+set_size_y(box_1, 300);
 set_x(box_1, -25);
 set_y(box_1, 25);
-set_rotation(box_1, 23);
+set_rotation_z(box_1, 23);
 DOM_box_1.style.backgroundColor = "rgb(60, 120, 185)";
 DOM_box_1.style.boxShadow = "10px 10px 20px rgb(130, 130, 130)";
 DOM_box_1.style.borderRadius = "50px";
 
 let box_2 = 1;
 let DOM_box_2 = DOM_element[box_2];
-set_width(box_2, 700);
-set_height(box_2, 700);
+set_size_x(box_2, 700);
+set_size_y(box_2, 700);
 set_x(box_2, 700);
 set_y(box_2, -350);
-set_rotation(box_2, 50);
+set_rotation_z(box_2, 50);
 DOM_box_2.style.backgroundColor = "rgb(255, 255, 255)";
 DOM_box_2.style.boxShadow = "10px 10px 25px rgb(175, 175, 175)";
 DOM_box_2.style.borderRadius = "150px";
 
 let box_3 = 2;
 let DOM_box_3 = DOM_element[box_3];
-set_width(box_3, 200);
-set_height(box_3, 200);
+set_size_x(box_3, 200);
+set_size_y(box_3, 200);
 set_x(box_3, 0);
 set_y(box_3, 900);
-set_rotation(box_3, 15);
+set_rotation_z(box_3, 15);
 DOM_box_3.style.backgroundColor = "rgb(255, 255, 255)";
 DOM_box_3.style.boxShadow = "10px 10px 25px rgb(175, 175, 175)";
 DOM_box_3.style.borderRadius = "25px";
 
 let box_4 = 3;
 let DOM_box_4 = DOM_element[box_4];
-set_width(box_4, 2000);
-set_height(box_4, 75);
-DOM_box_4.style.zIndex = "2";
+set_size_x(box_4, 2000);
+set_size_y(box_4, 75);
+set_z(box_4, 1);
 DOM_box_4.style.backgroundColor = "rgb(255, 255, 255)";
 DOM_box_4.style.boxShadow = "1px 0px 10px rgb(75, 75, 75)";
 
-
-
-function translate_x() {}
-function translate_y() {}
-function rotate() {}
 
 
 function lite() {
