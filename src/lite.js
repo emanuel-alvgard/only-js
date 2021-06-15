82e70d551ded440441bf48d2083a94c0796d7016
emanuel-alvgard <emanuel.alvgard@gmail.com>
2021-06-15 12:21:49 +0200

    -

--- src/lite.js
+++ src/lite.js
@@ -29,30 +29,55 @@ DOM_body.append(DOM_ROOT);
 
 
 // ELEMENT
-let virtual_size = 0;
 let DOM_element = [];
-let update;
-
+let virtual_size = 0;
 let id;
-let width;
-let height;
 
-let translation_x;
-let translation_y;
+let width;
+let width_u;
+
+let height;
+let height_u;
+
+let x;
+let y;
 let rotation;
+let matrix_u;
+
+
+
+function get_width(id) { return width[id]; }
+function set_width(id, value) { width[id] = value; width_u[id] = 1; }
+
+function get_height(id) { return height[id]; }
+function set_height(id, value) { height[id] = value; height_u[id] = 1; }
+
+function get_x(id) { return x[id]; }
+function set_x(id, value) { x[id] = value; matrix_u[id] = 1; }
+
+function get_y(id) { return y[id]; }
+function set_y(id, value) { y[id] = value; matrix_u[id] = 1; }
+
+function get_rotation(id) { return rotation[id]; }
+function set_rotation(id, value) { rotation[id] = value; matrix_u[id] = 1; }
+
 
 
 function create_virtual(size) {
 
     virtual_size = size;
-    update = new Int8Array(size);
-
     id = new Int32Array(size);
+
     width = new Int32Array(size);
+    width_u = new Int8Array(size);
+    
     height = new Int32Array(size);
-    translation_x = new Int32Array(size);
-    translation_y = new Int32Array(size);
+    height_u = new Int8Array(size);
+    
+    x = new Int32Array(size);
+    y = new Int32Array(size);
     rotation = new Int32Array(size);
+    matrix_u = new Int8Array(size);
 
     let i = 0;
     while (i < size) { DOM_element[i] = document.createElement("div"); i += 1; } i = 0;
@@ -69,88 +94,101 @@ function clear_virtual() {
     
     let i = 0;
     while (i < virtual_size) { width[i] = 0; i += 1; } i = 0;
-    while (i < virtual_size) { height[i] = 0; i += 1; } i = 0;
-    while (i < virtual_size) { translation_x[i] = 0; i += 1; } i = 0;
-    while (i < virtual_size) { translation_y[i] = 0; i += 1; } i = 0;
-    while (i < virtual_size) { rotation[i] = 0; i += 1; } i = 0;
+    while (i < virtual_size) { width_u[i] = 1; i += 1; } i = 0;
 
-    while (i < virtual_size) { update[i] = 1; i += 1; }
+    while (i < virtual_size) { height[i] = 0; i += 1; } i = 0;
+    while (i < virtual_size) { height_u[i] = 1; i += 1; } i = 0;
+
+    while (i < virtual_size) { x[i] = 0; i += 1; } i = 0;
+    while (i < virtual_size) { y[i] = 0; i += 1; } i = 0;
+    while (i < virtual_size) { rotation[i] = 0; i += 1; } i = 0;
+    while (i < virtual_size) { matrix_u[i] = 1; i += 1; } i = 0;
 }
 
 
 
 function update_DOM() {
 
-    let i = 0;
+    let i; 
+    
+    i = 0;
     while (i < virtual_size) {
-        if (update[i] === 1) { 
-            DOM_element[i].style.width = width[i] + "px";  
+        if (width_u[i] === 1) { 
+            DOM_element[i].style.width = width[i] + "px";
+            width_u[i] = 0;  
         } i += 1;
-    } i = 0;
+    } 
+    
+    i = 0;
     while (i < virtual_size) {
-        if (update[i] === 1) { 
-            DOM_element[i].style.height = height[i] + "px"; 
+        if (height_u[i] === 1) { 
+            DOM_element[i].style.height = height[i] + "px";
+            height_u[i] = 0; 
         } i += 1;
-    } i = 0;
+    } 
+    
+    i = 0;
     while (i < virtual_size) {
-        if (update[i] === 1) { 
+        if (matrix_u[i] === 1) { 
             DOM_element[i].style.transform = "translate(" 
-            + translation_x[i] + "px, "
-            + translation_y[i] + "px) "
-            + "rotate(" + rotation[i] + "deg)"; 
+            + x[i] + "px, "
+            + y[i] + "px) "
+            + "rotate(" + rotation[i] + "deg)";
+            matrix_u[i] = 0; 
         } i += 1;
     } i = 0;
-
-    while (i < virtual_size) { update[i] = 0; i += 1; }
 }
+
+
 
 create_virtual(100);
 
 let box_1 = 0;
 let DOM_box_1 = DOM_element[box_1];
-width[box_1] = 300;
-height[box_1] = 300;
-translation_x[box_1] = -25;
-translation_y[box_1] = 25;
-rotation[box_1] = 23;
+set_width(box_1, 300);
+set_height(box_1, 300);
+set_x(box_1, -25);
+set_y(box_1, 25);
+set_rotation(box_1, 23);
 DOM_box_1.style.backgroundColor = "rgb(60, 120, 185)";
 DOM_box_1.style.boxShadow = "10px 10px 20px rgb(130, 130, 130)";
 DOM_box_1.style.borderRadius = "50px";
-update[box_1] = 1;
 
 let box_2 = 1;
 let DOM_box_2 = DOM_element[box_2];
-width[box_2] = 700;
-height[box_2] = 700;
-translation_x[box_2] = 700;
-translation_y[box_2] = -350;
-rotation[box_2] = 50;
+set_width(box_2, 700);
+set_height(box_2, 700);
+set_x(box_2, 700);
+set_y(box_2, -350);
+set_rotation(box_2, 50);
 DOM_box_2.style.backgroundColor = "rgb(255, 255, 255)";
 DOM_box_2.style.boxShadow = "10px 10px 25px rgb(175, 175, 175)";
 DOM_box_2.style.borderRadius = "150px";
-update[box_2] = 1;
 
 let box_3 = 2;
 let DOM_box_3 = DOM_element[box_3];
-width[box_3] = 200;
-height[box_3] = 200;
-translation_x[box_3] = 0;
-translation_y[box_3] = 900;
-rotation[box_3] = 15;
+set_width(box_3, 200);
+set_height(box_3, 200);
+set_x(box_3, 0);
+set_y(box_3, 900);
+set_rotation(box_3, 15);
 DOM_box_3.style.backgroundColor = "rgb(255, 255, 255)";
 DOM_box_3.style.boxShadow = "10px 10px 25px rgb(175, 175, 175)";
 DOM_box_3.style.borderRadius = "25px";
-update[box_3] = 1;
-
 
 let box_4 = 3;
 let DOM_box_4 = DOM_element[box_4];
-width[box_4] = 2000;
-height[box_4] = 75;
+set_width(box_4, 2000);
+set_height(box_4, 75);
 DOM_box_4.style.zIndex = "2";
 DOM_box_4.style.backgroundColor = "rgb(255, 255, 255)";
 DOM_box_4.style.boxShadow = "1px 0px 10px rgb(75, 75, 75)";
-update[box_4] = 1;
+
+
+
+function translate_x() {}
+function translate_y() {}
+function rotate() {}
 
 
 function lite() {
