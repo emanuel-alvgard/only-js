256750e989b385fe884a43888480ebf9cc4947b5
emanuel-alvgard <emanuel.alvgard@gmail.com>
2021-06-16 10:21:03 +0200

    -

--- src/lite.js
+++ src/lite.js
@@ -57,6 +57,7 @@ let DOM_element = [];
 let virtual_size = 0;
 let id;
 
+// PROPERTY
 let size_x;
 let size_x_u;
 
@@ -77,11 +78,11 @@ let mousedown;
 let mouseup;
 
 // ACTION
-let draggable;
-let dragged;
+let drag;
 
 
-// GET/SET
+
+// GET/SET PROPERTY
 function get_size_x(id) { return size_x[id]; }
 function set_size_x(id, value) { size_x[id] = value; size_x_u[id] = 1; }
 
@@ -101,6 +102,17 @@ function get_z(id) { return z[id]; }
 function set_z(id, value) { z[id] = value; z_u[id] = 1; }
 
 
+
+// GET EVENT
+function get_mousemove(id) { return mousemove[id]; }
+function get_mousedown(id) { return mousedown[id]; }
+function get_mouseup(id) { return mouseup[id]; }
+
+// GET ACTION
+function get_drag(id) { return drag[id]; }
+
+
+
 // EVENT
 function event_mousemove(event) { mousemove[+event["srcElement"]["id"]] = 1; }
 function event_mousedown(event) { mousedown[+event["srcElement"]["id"]] = 1; }
@@ -110,15 +122,9 @@ function add_event_mousemove(id) { DOM_element[id].addEventListener("mousemove",
 function add_event_mousedown(id) { DOM_element[id].addEventListener("mousedown", event_mousedown); }
 function add_event_mouseup(id) { DOM_element[id].addEventListener("mouseup", event_mouseup); }
 
-// ACTION
-function drag() {}
-
-// ANIMATION
-function curve_animation() {}
-function animation_x() {}
-function animation_y() {}
-function animation_rotation_z() {}
-function animate_z() {}
+function remove_event_mousemove(id) { DOM_element[id].removeEventListener("mousemove", event_mousemove); }
+function remove_event_mousedown(id) { DOM_element[id].removeEventListener("mousedown", event_mousedown); }
+function remove_event_mouseup(id) { DOM_element[id].removeEventListener("mouseup", event_mouseup); }
 
 
 
@@ -127,6 +133,7 @@ function create_virtual(size) {
     virtual_size = size;
     id = new Int32Array(size);
 
+    // PROPERTY
     size_x = new Int32Array(size);
     size_x_u = new Int32Array(size);
     
@@ -141,9 +148,13 @@ function create_virtual(size) {
     z = new Int32Array(size);
     z_u = new Int32Array(size);
 
-    mousemove = new Uint32Array(size);
-    mousedown = new Uint32Array(size);
-    mouseup = new Uint32Array(size);
+    // EVENT
+    mousemove = new Int32Array(size);
+    mousedown = new Int32Array(size);
+    mouseup = new Int32Array(size);
+
+    // ACTION
+    drag = new Int32Array(size);
 
     let i = 0;
     while (i < size) { DOM_element[i] = document.createElement("div"); i += 1; } i = 0;
@@ -171,6 +182,16 @@ function clear_virtual() {
 
     while (i < virtual_size) { z[i] = 0; i += 1; } i = 0;
     while (i < virtual_size) { z_u[i] = 1; i += 1; } i = 0;
+    
+    while (i < virtual_size) { mousemove[i] = 0; i += 1; } i = 0;
+    while (i < virtual_size) { mousedown[i] = 0; i += 1; } i = 0;
+    while (i < virtual_size) { mouseup[i] = 0; i += 1; } i = 0;
+
+    while (i < virtual_size) { drag[i] = 0; i += 1; } i = 0;
+
+    while (i < virtual_size) { remove_event_mousemove(i); i += 1; } i = 0;
+    while (i < virtual_size) { remove_event_mousedown(i); i += 1; } i = 0;
+    while (i < virtual_size) { remove_event_mouseup(i); i += 1; } i = 0;
 }
 
 
@@ -181,37 +202,37 @@ function update_DOM() {
     
     i = 0;
     while (i < virtual_size) {
-        if (size_x_u[i] === 1) { 
-            DOM_element[i].style.width = size_x[i] + "px";
-            size_x_u[i] = 0;  
-        } i += 1;
+        if (size_x_u[i] === 0) { i += 1; continue; } 
+        DOM_element[i].style.width = size_x[i] + "px";
+        size_x_u[i] = 0;  
+        i += 1;
     } 
     
     i = 0;
     while (i < virtual_size) {
-        if (size_y_u[i] === 1) { 
-            DOM_element[i].style.height = size_y[i] + "px";
-            size_y_u[i] = 0; 
-        } i += 1;
+        if (size_y_u[i] === 0) { i += 1; continue; } 
+        DOM_element[i].style.height = size_y[i] + "px";
+        size_y_u[i] = 0; 
+        i += 1;
     } 
     
     i = 0;
     while (i < virtual_size) {
-        if (transform_u[i] === 1) { 
-            DOM_element[i].style.transform = "translate(" 
-            + x[i] + "px, "
-            + y[i] + "px) "
-            + "rotate(" + rotation_z[i] + "deg)";
-            transform_u[i] = 0; 
-        } i += 1;
+        if (transform_u[i] === 0) { i += 1; continue; } 
+        DOM_element[i].style.transform = "translate(" 
+        + x[i] + "px, "
+        + y[i] + "px) "
+        + "rotate(" + rotation_z[i] + "deg)";
+        transform_u[i] = 0; 
+        i += 1;
     }
 
     i = 0;
     while (i < virtual_size) {
-        if (z_u[i] === 1) { 
-            DOM_element[i].style.zIndex = z[i] + "";
-            z_u[i] = 0; 
-        } i += 1;
+        if (z_u[i] === 0) { i += 1; continue; } 
+        DOM_element[i].style.zIndex = z[i] + "";
+        z_u[i] = 0; 
+        i += 1;
     } 
 
     
@@ -226,7 +247,52 @@ function update_DOM() {
 
 
 
-create_virtual(100);
+
+
+// ANIMATION
+function animation() {}
+function animation_x() {}
+function animation_y() {}
+function animation_rotation_z() {}
+function animate_z() {}
+
+
+
+
+// ACTION
+function action_drag() {    
+    
+    let _x = ROOT_mouse_x - ROOT_mouse_x_previous;
+    let _y = ROOT_mouse_y - ROOT_mouse_y_previous;
+    
+    let i = 0;
+    while (i < virtual_size) {
+        if (drag[i] === 0) { i += 1; continue; }
+        if (mousedown[i] === 1) { drag[i] = 2; }
+        if (drag[i] === 2 && ROOT_mousemove === 1) {
+            set_x(i, x[i] + _x);
+            set_y(i, y[i] + _y);
+        }
+        if (ROOT_mouseup === 1) { drag[i] = 1; }
+        i += 1;
+    }
+}
+
+function add_action_drag(id) { drag[id] = 1; }
+
+function remove_action_drag(id) { drag[id] = 0; }
+
+function update_ACTION() {
+    action_drag();
+}
+
+
+
+
+
+
+
+create_virtual(10000);
 
 
 
@@ -241,6 +307,7 @@ DOM_box_1.style.backgroundColor = "rgb(60, 120, 185)";
 DOM_box_1.style.boxShadow = "10px 10px 20px rgb(130, 130, 130)";
 DOM_box_1.style.borderRadius = "50px";
 add_event_mousedown(box_1);
+add_action_drag(box_1);
 
 let box_2 = 1;
 let DOM_box_2 = DOM_element[box_2];
@@ -252,6 +319,8 @@ set_rotation_z(box_2, 50);
 DOM_box_2.style.backgroundColor = "rgb(255, 255, 255)";
 DOM_box_2.style.boxShadow = "10px 10px 25px rgb(175, 175, 175)";
 DOM_box_2.style.borderRadius = "150px";
+add_event_mousedown(box_2);
+add_action_drag(box_2);
 
 let box_3 = 2;
 let DOM_box_3 = DOM_element[box_3];
@@ -276,7 +345,8 @@ let drag_test = 0;
 
 function lite() {
 
-    // TEST DRAG
+    /* TEST DRAG
+    
     if (mousedown[box_1] === 1) { drag_test = 1; }
     if (drag_test === 1 && ROOT_mousemove === 1) {
         let new_x = ROOT_mouse_x - ROOT_mouse_x_previous;
@@ -285,10 +355,9 @@ function lite() {
         set_y(box_1, get_y(box_1) + new_y);
     }
     if (ROOT_mouseup === 1) { drag_test = 0; }
-    
-    
-    // TEST ANIMATION? 
+    */
 
+    update_ACTION();
     update_DOM();
     return window.requestAnimationFrame(lite);
 }
