21a45fa4686a4b171749253608ba18664add3804
emanuel-alvgard <emanuel.alvgard@gmail.com>
2021-06-17 10:49:27 +0200

    -

--- src/lite.js
+++ src/lite.js
@@ -1,5 +1,15 @@
 "use strict";
 
+// DEVICE
+let device_touch = 0;
+
+function device_information() {
+    if (navigator.maxTouchPoints > 0) { device_touch = 1; }
+}
+
+device_information();
+
+
 /*-------------
     DOM BODY
 ---------------*/
@@ -28,20 +38,28 @@ ROOT.style.backgroundColor = "white";
 DOM_body.append(ROOT);
 
 let ROOT_mousemove = 0;
-let ROOT_mouse_x_previous = 0;
-let ROOT_mouse_y_previous = 0;
 let ROOT_mouse_x = 0;
 let ROOT_mouse_y = 0;
-
+let ROOT_mouse_x_previous = 0;
+let ROOT_mouse_y_previous = 0;
 let ROOT_mousedown = 0;
 let ROOT_mouseup = 0;
 
+let ROOT_touchstart = 0;
+let ROOT_touchmove = 0;
+let ROOT_touch_x = 0;
+let ROOT_touch_y = 0;
+let ROOT_touch_x_previous = 0;
+let ROOT_touch_y_previous = 0;
+let ROOT_touchend = 0;
+
+// MOUSE
 function ROOT_event_mousemove(event) { 
     ROOT_mousemove = 1;
     ROOT_mouse_x_previous = ROOT_mouse_x;
     ROOT_mouse_y_previous = ROOT_mouse_y;
-    ROOT_mouse_x = event["clientX"];
-    ROOT_mouse_y = event["clientY"];
+    ROOT_mouse_x = event.clientX;
+    ROOT_mouse_y = event.clientY;
 }
 function ROOT_event_mousedown(event) { ROOT_mousedown = 1; }
 function ROOT_event_mouseup(event) { ROOT_mouseup = 1; }
@@ -49,6 +67,23 @@ function ROOT_event_mouseup(event) { ROOT_mouseup = 1; }
 ROOT.addEventListener("mousemove", ROOT_event_mousemove);
 ROOT.addEventListener("mousedown", ROOT_event_mousedown);
 ROOT.addEventListener("mouseup", ROOT_event_mouseup);
+
+// TOUCH
+function ROOT_event_touchstart(event) { ROOT_touchstart = 1; }
+function ROOT_event_touchmove(event) {
+    ROOT_touchmove = 1;
+    ROOT_touch_x_previous = ROOT_touch_x;
+    ROOT_touch_y_previous = ROOT_touch_y;
+    ROOT_touch_x = event.touches[0].clientX;
+    ROOT_touch_y = event.touches[0].clientY;
+}
+function ROOT_event_touchend(event) { ROOT_touchend = 1; }
+
+ROOT.addEventListener("touchstart", ROOT_event_touchstart);
+ROOT.addEventListener("touchmove", ROOT_event_touchmove);
+ROOT.addEventListener("touchend", ROOT_event_touchend);
+
+
 
 // PAGE
 let page = new Int32Array(100);
@@ -78,6 +113,10 @@ let z_u;
 let mousemove;
 let mousedown;
 let mouseup;
+
+let touchstart;
+let touchmove;
+let touchend;
 
 // LINKED
 let link_x;
@@ -118,6 +157,10 @@ function get_mousemove(id) { return mousemove[id]; }
 function get_mousedown(id) { return mousedown[id]; }
 function get_mouseup(id) { return mouseup[id]; }
 
+function get_touchstart(id) { return touchstart[id]; }
+function get_touchmove(id) { return touchmove[id]; }
+function get_touchend(id) { return touchend[id]; }
+
 // GET LINKED
 
 // GET ANIMATION
@@ -128,9 +171,9 @@ function get_drag(id) { return drag[id]; }
 
 
 // EVENT
-function event_mousemove(event) { mousemove[+event["srcElement"]["id"]] = 1; }
-function event_mousedown(event) { mousedown[+event["srcElement"]["id"]] = 1; }
-function event_mouseup(event) { mouseup[+event["srcElement"]["id"]] = 1; }
+function event_mousemove(event) { mousemove[event.srcElement.id] = 1; }
+function event_mousedown(event) { mousedown[event.srcElement.id] = 1; }
+function event_mouseup(event) { mouseup[event.srcElement.id] = 1; }
 
 function add_event_mousemove(id) { DOM_element[id].addEventListener("mousemove", event_mousemove); }
 function add_event_mousedown(id) { DOM_element[id].addEventListener("mousedown", event_mousedown); }
@@ -140,6 +183,17 @@ function remove_event_mousemove(id) { DOM_element[id].removeEventListener("mouse
 function remove_event_mousedown(id) { DOM_element[id].removeEventListener("mousedown", event_mousedown); }
 function remove_event_mouseup(id) { DOM_element[id].removeEventListener("mouseup", event_mouseup); }
 
+function event_touchstart(event) { touchstart[event.srcElement.id] = 1; }
+function event_touchmove(event) { touchmove[event.srcElement.id] = 1; }
+function event_touchend(event) { touchend[event.srcElement.id] = 1; }
+
+function add_event_touchstart(id) { DOM_element[id].addEventListener("touchstart", event_touchstart); }
+function add_event_touchmove(id) { DOM_element[id].addEventListener("touchmove", event_touchmove); }
+function add_event_touchend(id) { DOM_element[id].addEventListener("touchend", event_touchend); }
+
+function remove_event_touchstart(id) { DOM_element[id].removeEventListener("touchstart", event_touchstart); }
+function remove_event_touchmove(id) { DOM_element[id].removeEventListener("touchmove", event_touchmove); }
+function remove_event_touchend(id) { DOM_element[id].removeEventListener("touchend", event_touchend); }
 
 
 function create_virtual(size) {
@@ -167,6 +221,10 @@ function create_virtual(size) {
     mousedown = new Int32Array(size);
     mouseup = new Int32Array(size);
 
+    touchstart = new Int32Array(size);
+    touchmove = new Int32Array(size);
+    touchend = new Int32Array(size);
+
     // LINKED
     link_x = new Int32Array(size);
     link_x_current = new Int32Array(size);
@@ -180,6 +238,7 @@ function create_virtual(size) {
     let i = 0;
     while (i < size) { DOM_element[i] = document.createElement("div"); i += 1; } i = 0;
     while (i < size) { DOM_element[i].style.position = "absolute"; i += 1; } i = 0;
+    while (i < size) { DOM_element[i].style.display = "none"; i += 1; } i = 0;
     while (i < size) { DOM_element[i].id = i; i += 1; } i = 0;
     while (i < size) { id[i] = i; i += 1; } i = 0;
     while (i < size) { link_x[i] = -1; i += 1; } i = 0;
@@ -209,6 +268,10 @@ function clear_virtual() {
     while (i < virtual_size) { mousedown[i] = 0; i += 1; } i = 0;
     while (i < virtual_size) { mouseup[i] = 0; i += 1; } i = 0;
 
+    while (i < virtual_size) { touchstart[i] = 0; i += 1; } i = 0;
+    while (i < virtual_size) { touchmove[i] = 0; i += 1; } i = 0;
+    while (i < virtual_size) { touchend[i] = 0; i += 1; } i = 0;
+
     while (i < virtual_size) { link_x[i] = -1; i += 1; } i = 0;
     while (i < virtual_size) { link_x_current[i] = -1; i += 1; } i = 0;
     while (i < virtual_size) { link_x_previous[i] = -1; i += 1; } i = 0;
@@ -220,6 +283,10 @@ function clear_virtual() {
     while (i < virtual_size) { remove_event_mousemove(i); i += 1; } i = 0;
     while (i < virtual_size) { remove_event_mousedown(i); i += 1; } i = 0;
     while (i < virtual_size) { remove_event_mouseup(i); i += 1; } i = 0;
+
+    while (i < virtual_size) { remove_event_touchstart(i); i += 1; } i = 0;
+    while (i < virtual_size) { remove_event_touchmove(i); i += 1; } i = 0;
+    while (i < virtual_size) { remove_event_touchend(i); i += 1; } i = 0;
 }
 
 
@@ -271,12 +338,17 @@ function update_DOM() {
     ROOT_mousemove = 0;
     ROOT_mousedown = 0;
     ROOT_mouseup = 0;
+    ROOT_touchstart = 0;
+    ROOT_touchmove = 0;
+    ROOT_touchend = 0;
 }
 
 
 
-// LINKED // NOT DONE
+// LINKED
 function linked_x() {
+    // @ERROR
+    // see if there's a way to set initial position independent of linking
     let i = 0;
     while (i < virtual_size) {
         if (link_x[i] === -1) { i += 1; continue; }
@@ -309,22 +381,50 @@ function animate_z() {}
 
 
 
-
 // ACTION
 function action_drag() {    
     
-    let _x = ROOT_mouse_x - ROOT_mouse_x_previous;
-    let _y = ROOT_mouse_y - ROOT_mouse_y_previous;
-    
+    let _x = 0;
+    let _y = 0;
+
+    if (device_touch === 0) {
+        _x = ROOT_mouse_x - ROOT_mouse_x_previous;
+        _y = ROOT_mouse_y - ROOT_mouse_y_previous;
+    }
+    else {
+        _x = ROOT_touch_x - ROOT_touch_x_previous;
+        _y = ROOT_touch_y - ROOT_touch_y_previous;
+    }
+
     let i = 0;
     while (i < virtual_size) {
+        if (device_touch === 1) { break; }
         if (drag[i] === 0) { i += 1; continue; }
+        
         if (mousedown[i] === 1) { drag[i] = 2; }
         if (drag[i] === 2 && ROOT_mousemove === 1) {
             set_x(i, x[i] + _x);
             set_y(i, y[i] + _y);
         }
         if (ROOT_mouseup === 1) { drag[i] = 1; }
+        i += 1;
+    }
+
+
+    // @ERROR
+    // after moving an element the first time it does'nt matter where the next touch happens
+    // it still moves the element
+    i = 0
+    while (i < virtual_size) {
+        if (device_touch === 0) { break; }
+        if (drag[i] === 0) { i += 1; continue; }
+        
+        if (touchstart[i] === 1) { drag[i] = 2; }
+        if (drag[i] === 2 && ROOT_touchmove === 1) {
+            set_x(i, x[i] + _x);
+            set_y(i, y[i] + _y);
+        }
+        if (ROOT_touchend === 1) { drag[i] = 1; }
         i += 1;
     }
 }
@@ -341,7 +441,7 @@ function update_ACTION() {
 
 
 
-create_virtual(1000);
+create_virtual(100);
 
 function create_home_page() {
     
@@ -357,6 +457,7 @@ function create_home_page() {
     let DOM_box_4 = DOM_element[box_4];
 
     // box_1
+    DOM_box_1.style.display = "initial";
     set_size_x(box_1, 300);
     set_size_y(box_1, 300);
     set_x(box_1, -25);
@@ -366,13 +467,17 @@ function create_home_page() {
     DOM_box_1.style.boxShadow = "10px 10px 20px rgb(130, 130, 130)";
     DOM_box_1.style.borderRadius = "50px";
     add_event_mousedown(box_1);
+    add_event_touchstart(box_1);
+    add_event_touchmove(box_1);
+    add_event_touchend(box_1);
     add_action_drag(box_1);
-    add_link_x(box_1, box_2);
+    //add_link_x(box_1, box_2);
 
     // box_2
+    DOM_box_2.style.display = "initial";
     set_size_x(box_2, 700);
     set_size_y(box_2, 700);
-    set_x(box_2, 700);
+    set_x(box_2, 500);
     set_y(box_2, -350);
     set_rotation_z(box_2, 50);
     DOM_box_2.style.backgroundColor = "rgb(255, 255, 255)";
@@ -382,21 +487,24 @@ function create_home_page() {
     add_action_drag(box_2);
 
     // box_3
+    DOM_box_3.style.display = "initial";
     set_size_x(box_3, 200);
     set_size_y(box_3, 200);
     set_x(box_3, 0);
-    set_y(box_3, 900);
+    set_y(box_3, 400);
     set_rotation_z(box_3, 15);
     DOM_box_3.style.backgroundColor = "rgb(255, 255, 255)";
     DOM_box_3.style.boxShadow = "10px 10px 25px rgb(175, 175, 175)";
     DOM_box_3.style.borderRadius = "25px";
 
-    // box_4
+    /* box_4
+    DOM_box_4.style.display = "initial";
     set_size_x(box_4, 2000);
     set_size_y(box_4, 75);
     set_z(box_4, 1);
     DOM_box_4.style.backgroundColor = "rgb(255, 255, 255)";
     DOM_box_4.style.boxShadow = "1px 0px 10px rgb(75, 75, 75)";
+    */
 }
 
 function create_other_page() {
@@ -406,6 +514,7 @@ function create_other_page() {
     let DOM_box_1 = DOM_element[box_1];
 
     // box_1
+    DOM_box_1.style.display = "initial";
     set_size_x(box_1, 300);
     set_size_y(box_1, 300);
     set_x(box_1, -25);
@@ -459,6 +568,8 @@ function router() {
 
 function lite() {
     
+    window.scrollTo(0, 0);
+
     router();
     update_ACTION();
     update_LINKED();
