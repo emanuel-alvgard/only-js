d0afda3a79552a1294eaa28da5020a41111fa706
emanuel-alvgard <emanuel.alvgard@crossnet.se>
2022-10-18 11:16:18 +0200

    -

---
+++ v.2/public/runtime/info.mjs
@@ -0,0 +1,5 @@
+// this will enable interactive programming on the client.
+// serialization will be done eventually which can later be used for developing an editor.
+
+// create a keyboard shortcut which brings up a window with runtime information?
+// runtime info must manually enabled in app.mjs
--- v.2/public/systems/test.mjs
+++ v.2/public/systems/test.mjs
@@ -1,1 +1,11 @@
 import * as runtime from "./runtime/runtime.mjs";
+
+let button;
+
+runtime.system(function(context) {
+
+    if (context.LOAD) {
+        button = element();
+    }
+
+});
