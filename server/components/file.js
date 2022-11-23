39e50164647257debbfd46ae7b5406e63fcf0c08
emanuel-alvgard <emanuel.alvgard@gmail.com>
2022-11-23 11:17:52 +0100

    -

--- server/components/file.js
+++ server/components/file.js
@@ -1,15 +1,15 @@
 const fs_builtin = require("fs");
 const util = require("../_runtime/tools/util.js");
 
+const types = {
+    js: "text/javascript",
+    mjs: "text/javascript",
+    html: "text/html",
+    svg: "image/svg+xml"
+}
+
 // @DONE
 exports.component = async (context, incoming) => { // @ADD config
-
-    const types = {
-        js: "text/javascript",
-        mjs: "text/javascript",
-        html: "text/html",
-        svg: "image/svg+xml"
-    }
 
     let path = "..";  
     let file_type = util.filetype(incoming.url);
